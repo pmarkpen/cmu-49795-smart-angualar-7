aa05ab2b5125a9c300cc041a1b4599c1ddc539ac
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2019-03-07 15:02:25 -0800

    change page to result when click a product

--- src/app/shopper/select-item/select-item.component.ts
+++ src/app/shopper/select-item/select-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import StoreItem from './model/store-item';
 import Product from './model/product';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-select-item',
@@ -8,31 +9,34 @@ import Product from './model/product';
   styleUrls: ['./select-item.component.scss']
 })
 export class SelectItemComponent implements OnInit {
-  
+
   storeList: StoreItem[];
   productList: Product[];
-  constructor() {
+  constructor(private router: Router) {
     let temp = new StoreItem();
     temp.storeId = 1;
     temp.storeName = "SafeWay";
     temp.storeDescription = "Cupertino";
 
-    this.storeList = [temp,temp,temp,temp,temp,temp,temp,temp,temp,temp,temp,temp,temp,temp, temp,temp,temp,temp,temp,temp,temp,temp,temp,temp,temp,temp,temp,temp];
+    this.storeList = [temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp, temp];
 
     let productTemp = new Product();
     productTemp.name = "SET 2 TEA TOWELS";
-    this.productList = [productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp,productTemp];
-   }
+    this.productList = [productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp, productTemp];
+  }
 
-   fetchStores() {
+  fetchStores() {
 
-   }
+  }
 
-   fetchProducts(storeId: number) {
+  fetchProducts(storeId: number) {
 
-   }
+  }
 
   ngOnInit() {
   }
 
+  onClickProduct(product: Product) {
+    this.router.navigateByUrl('shopper/association-result');
+  }
 }
