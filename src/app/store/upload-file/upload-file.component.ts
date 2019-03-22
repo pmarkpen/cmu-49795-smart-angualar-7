23f1ba6c512a6b4fbf2cf87ace2e5dbce41ac87f
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2019-03-21 22:19:38 -0700

    upload file to google firebase

--- src/app/store/upload-file/upload-file.component.ts
+++ src/app/store/upload-file/upload-file.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { S3FileUploaderService } from './s3-file-uploader.service';
 import { Router } from '@angular/router';
 
+function getWindow(): any {
+  return window;
+}
+
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
@@ -9,19 +13,67 @@ import { Router } from '@angular/router';
 })
 export class UploadFileComponent implements OnInit {
 
-  selectedFiles:any;
+  selectedFiles: any;
   constructor(private s3FileUploaderService: S3FileUploaderService, private router: Router) { }
 
   ngOnInit() {
   }
 
   upload() {
+    var window = getWindow();
+    var firebase = window.firebase;
+    // Get a reference to the storage service, which is used to create references in your storage bucket
+    var storage = firebase.storage();
+    // Create a storage reference from our storage service
+    var storageRef = storage.ref();
+
     const file = this.selectedFiles.item(0);
-    this.s3FileUploaderService.uploadfile(file).subscribe((data) => {
-      this.router.navigateByUrl('store/store-association-result');
-    });
+    // Create the file metadata
+    var metadata = {
+      contentType: 'csv'
+    };
+
+    // Upload file and metadata to the object 'images/mountains.jpg'
+    var uploadTask = storageRef.child('files/' + file.name).put(file, metadata);
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+      function (snapshot) {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, function (error) {
+
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            break;
+          case 'storage/canceled':
+            // User canceled the upload
+            break;
+          case 'storage/unknown':
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      }, function () {
+        // Upload completed successfully, now we can get the download URL
+        var gsReference = storage.refFromURL(`gs://smart-46249.appspot.com/files/${file.name}`)
+        gsReference.getDownloadURL().then(function (downloadURL) {
+          console.log('File available at', downloadURL);
+        });
+      });
   }
- 
+
   selectFile(event) {
     this.selectedFiles = event.target.files;
   }
