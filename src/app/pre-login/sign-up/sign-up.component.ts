5c80c200fd6dacdf2455642b53071aa6e070b412
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2019-03-30 16:07:51 -0700

    done for sign-up store

--- src/app/pre-login/sign-up/sign-up.component.ts
+++ src/app/pre-login/sign-up/sign-up.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {MatFormFieldControl} from '@angular/material';
+import { MatFormFieldControl } from '@angular/material';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -13,42 +16,72 @@ export class SignUpComponent implements OnInit {
   confirmedPassword: string;
   storeName: string;
   storeDescription: string;
-  constructor() { }
+  constructor(private http: HttpClient, private route: Router) { }
 
   ngOnInit() {
   }
 
   onClickSignUp() {
-    if(this.isEmpty(this.storeId)){
+    if (this.isEmpty(this.storeId)) {
       alert("Store Id is required");
       return;
     }
-    if(this.isEmpty(this.password)){
+    if (this.isEmpty(this.password)) {
       alert("Password is required");
       return;
     }
-    if(this.isEmpty(this.confirmedPassword)){
+    if (this.isEmpty(this.confirmedPassword)) {
       alert("Confirm Password is required");
       return;
     }
-    if(this.password !== this.confirmedPassword){
+    if (this.password !== this.confirmedPassword) {
       alert("Password and Confirm Password must be the same");
       return;
     }
-    if(this.isEmpty(this.storeName)){
+    if (this.isEmpty(this.storeName)) {
       alert("Store Name is required");
       return;
     }
-    if(this.isEmpty(this.storeDescription)){
+    if (this.isEmpty(this.storeDescription)) {
       alert("Store Description is required");
       return;
     }
+
+    this.http.post(`http://localhost:3000/signup-store`, {
+      storeID: this.storeId,
+      password: this.password,
+      name: this.storeName,
+      description: this.storeDescription
+    }).subscribe((response: SignUpStoreResponse) => {
+      alert("Sign Up succesfully");
+      this.route.navigateByUrl("/");
+    }, (err) => {
+      alert(err.error.errors.reduce((previousValue, currentValue) => {
+        return `${previousValue}, ${currentValue}`
+      }));
+    });
+
   }
 
   isEmpty(value: string) {
     return value === undefined || value === "";
   }
 
-  
 
+
+
+
+}
+
+
+interface SignUpStoreResponse {
+  "status": string,
+  "result": {
+    "addedStore": {
+      "storeID": string,
+      "name": string,
+      "description": string
+    }
+  },
+  "errors": string[]
 }
