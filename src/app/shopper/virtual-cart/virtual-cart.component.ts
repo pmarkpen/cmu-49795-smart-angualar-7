be10cc29368e5316bd57635420751fab10244e6c
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2019-04-08 14:25:38 -0700

    fetch virtual cart

--- src/app/shopper/virtual-cart/virtual-cart.component.ts
+++ src/app/shopper/virtual-cart/virtual-cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ShopperInformationService } from '../../shopper-information.service';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-virtual-cart',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VirtualCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shopperInformationService: ShopperInformationService, private http: HttpClient) { }
 
+  items: GetVirtualCartResponseItem[];
   ngOnInit() {
+    this.initializeTheTable();
   }
 
+  fetchVirtualCart(): Observable<GetVirtualCartResponse> {
+    return this.http.get(`http://localhost:3000/api/virtual-cart/${this.shopperInformationService.shopperId}`) as Observable<GetVirtualCartResponse>; 
+  }
+
+  initializeTheTable() {
+    this.fetchVirtualCart().subscribe((result: GetVirtualCartResponse) => {
+      this.items = result.result.requests;
+    });
+  }
+}
+
+interface GetVirtualCartResponse {
+  "status": string;
+  "result": {
+    "requests": GetVirtualCartResponseItem[]
+  }
+}
+
+interface GetVirtualCartResponseItem {
+  "id": string;
+  "shopperID": string;
+  "productName": string;
+  "storeName": string;
 }
