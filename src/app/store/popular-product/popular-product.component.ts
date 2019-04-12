103e5dfb94d75d9c3107346af36ae9685e5e2df9
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2019-04-12 10:15:40 -0700

    successfully add chart

--- src/app/store/popular-product/popular-product.component.ts
+++ src/app/store/popular-product/popular-product.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import Product from '../../shopper/select-item/model/product';
 import FetchProductResponse from '../../shopper/select-item/model/fetch-product-response';
-import {environment} from '../../../environments/environment';
+import { environment } from '../../../environments/environment';
 import { StoreInformationService } from '../../store-information.service';
+import Chart from 'chart.js';
 
 @Component({
   selector: 'app-popular-product',
@@ -13,16 +14,19 @@ import { StoreInformationService } from '../../store-information.service';
 export class PopularProductComponent implements OnInit {
   productList = [];
   filterValue: string;
+  @ViewChild('myChart') myChart;
+
   constructor(private http: HttpClient, private storeInformationService: StoreInformationService) { }
 
   ngOnInit() {
     this.fetchProducts(this.storeInformationService.storeId);
+    this.drawChart();
   }
 
   fetchProducts(storeId: string) {
     this.productList = [];
     this.http.get(`http://${environment.host}/api/stores/${storeId}`).subscribe((response: FetchProductResponse) => {
-      if(response.result.requests.length >= 1) {
+      if (response.result.requests.length >= 1) {
         response.result.requests[0].products.sort();
         response.result.requests[0].products.forEach((uniqueProductName) => {
           let pItem = new Product();
@@ -36,6 +40,33 @@ export class PopularProductComponent implements OnInit {
   doFilter(value: string) {
     this.filterValue = value;
   }
+
+
+  drawChart() {
+    let data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+        label: 'My First dataset',
+        data: [0, 1, 2, 3, 4, 5, 6]
+      }]
+    };
+
+    var myLineChart = new Chart(this.myChart.nativeElement, {
+      type: 'line',
+      data: data,
+      options: {
+        layout: {
+            padding: {
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 50
+            }
+        }
+      }
+    });
+  }
+
 
 
 }
