0ee48dce8cc9134ada842756ad6c7280e07829cf
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2019-05-18 16:09:39 -0700

    add when a service returns Observable.error

--- src/app/pre-login/login/login.component.spec.ts
+++ src/app/pre-login/login/login.component.spec.ts
@@ -6,13 +6,16 @@ import { RouterModule, ActivatedRoute, Router } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginService, LogInStoreResponse } from './login.service';
 import { Observable, of } from 'rxjs';
+import { throwError } from 'rxjs';
+import { StoreInformationService } from '../../shared-service/store-information.service';
 
 fdescribe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let loginService: LoginService;
   let router: Router;
-
+  let storeInformationService: StoreInformationService;
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterModule.forRoot([]),
@@ -28,6 +31,7 @@ fdescribe('LoginComponent', () => {
     fixture.detectChanges();
     loginService = TestBed.get(LoginService);
     router = TestBed.get(Router);
+    storeInformationService = TestBed.get(StoreInformationService);
   });
 
   it('should create', () => {
@@ -73,6 +77,34 @@ fdescribe('LoginComponent', () => {
 
           component.onClickLogin();
           expect(loginService.loginAsStore).toHaveBeenCalledWith(MOCK_USERNAME, MOCK_PASSWORD);
+        });
+
+        it("should keep users'information after successfully logging in with signin-api", () => {
+          spyOn(loginService, "loginAsStore").and.returnValue(of({
+            "status": "OK",
+            "result": {
+              "id": "12345",
+              "storeID": "walmart01",
+              "storeName": "Walmart",
+              "storeDescription": "Description"
+            }
+          }));
+          spyOn(storeInformationService, "setInformation");
+
+          component.onClickLogin();
+          expect(storeInformationService.setInformation).toHaveBeenCalledWith(true, "walmart01", "Walmart");
+        });
+
+        it("should show error when logging in was unsuccessful", () => {
+          spyOn(loginService, "loginAsStore").and.returnValue(throwError({
+            error: {
+              errors : ["Error1", "Error2"]
+            }
+          }));
+          spyOn(window, "alert");
+
+          component.onClickLogin();
+          expect(window.alert).toHaveBeenCalledWith("Error1, Error2");
         });
       });
     });
